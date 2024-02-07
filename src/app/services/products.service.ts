0fe7e9f28dad4ee22fafa21b6613d859b01ecf6a
Cian McKenna <cian.mck20@gmail.com>
2024-02-07 12:29:53 +0000

    Types Brought In

---
+++ src/app/services/products.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { Observable } from 'rxjs';
+import { PageinationParams, Products } from '../../types';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  constructor( private apiService: ApiService) { }
+
+  getProducts = (url: string, params: PageinationParams ): Observable<Products> => {
+    return this.apiService.get(url, {
+      params,
+      responseType: 'json',
+    });
+  }
+}
