5433e0ea840dab497ee9631d30f4aec80c11969c
MK-CIAN <cian.mck20@gmail.com>
2024-02-09 14:06:56 +0000

    Product Working

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { ProductsService } from '../services/products.service';
-import { Products } from '../../types';
+import { Product, Products } from '../../types';
+import { ProductComponent } from '../components/product/product.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ProductComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -13,9 +15,11 @@ import { Products } from '../../types';
 export class HomeComponent {
   constructor( private productsService: ProductsService) { }
 
+  products: Product[] = [];
+
   ngOnInit(): void {
     this.productsService.getProducts('http://localhost:3000/clothes', {page: 0, perPage: 5}).subscribe((products: Products) => {
-      console.log(products.items);
+      this.products = products.items;
     });
   }
 }
