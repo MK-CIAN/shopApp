68c9ca86b2a91920bbeb89a215055bf9421c92e7
MK-CIAN <cian.mck20@gmail.com>
2024-02-12 11:33:04 +0000

    Pagination Working

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,11 +3,12 @@ import { ProductsService } from '../services/products.service';
 import { Product, Products } from '../../types';
 import { ProductComponent } from '../components/product/product.component';
 import { CommonModule } from '@angular/common';
+import { PaginatorModule } from 'primeng/paginator';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductComponent, CommonModule],
+  imports: [ProductComponent, CommonModule, PaginatorModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -17,13 +18,24 @@ export class HomeComponent {
 
   products: Product[] = [];
 
+  totalRecords: number = 0;
+
   onProductOutput(product: Product) {
     console.log(product, 'Output');
   }
 
-  ngOnInit(): void {
-    this.productsService.getProducts('http://localhost:3000/clothes', {page: 0, perPage: 5}).subscribe((products: Products) => {
+  onPageChange(event: any) {
+    this.fetchProducts(event.page, event.rows);
+  }
+
+  fetchProducts(page: number, perPage: number) {
+    this.productsService.getProducts('http://localhost:3000/clothes', {page, perPage}).subscribe((products: Products) => {
       this.products = products.items;
+      this.totalRecords = products.total;
     });
+  }
+
+  ngOnInit(): void {
+    this.fetchProducts(0,5);
   }
 }
