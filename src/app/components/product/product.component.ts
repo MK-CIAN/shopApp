c300618ed756ea65e5f4d2c993e6ef2b18886ad8
MK-CIAN <cian.mck20@gmail.com>
2024-02-16 15:58:53 +0000

    Deleting Products & Editing Projects Functional

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -3,15 +3,20 @@ import { Product } from '../../../types';
 import { RatingModule } from 'primeng/rating';
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmationService } from 'primeng/api';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [ RatingModule, FormsModule, ButtonModule],
+  imports: [ RatingModule, FormsModule, ButtonModule, ConfirmPopupModule, ToastModule],
+  providers: [ConfirmationService],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
 export class ProductComponent {
+  constructor(private confirmationService: ConfirmationService) {}
   @Input() product!: Product;
   @Output() edit: EventEmitter<Product> = new EventEmitter<Product>();
   @Output() delete: EventEmitter<Product> = new EventEmitter<Product>();
@@ -21,6 +26,15 @@ export class ProductComponent {
     this.edit.emit(this.product);
   }
 
+  confirmDelete() {
+    this.confirmationService.confirm({
+      message: 'Are you sure that you want to delete this product?',
+      accept: () => {
+        this.deleteProduct();
+      }
+    });
+  }
+  
   deleteProduct() {
     this.delete.emit(this.product);
   }
